5546a0778bf45394e66151e5a8ea74be1d25eeba
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2022-10-28 12:43:00 +0200

    Create Exercice 8.py

---
+++ Exercice 8.py
@@ -0,0 +1,12 @@
+n1 = int(input("Donner Un nombre n1 : "))
+n2 = int(input("Donner Un nombre n2: "))
+d = (input("Donner Un operateur : "))
+match d:
+    case "+":
+        print(n1 + n2)
+    case "-":
+        print(n1 - n2)
+    case "*":
+        print(n1 * n2)
+    case "/":
+        print(n1 / n2)

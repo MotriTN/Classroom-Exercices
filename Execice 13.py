d58e44839c572b4a15f15dc0c4feb8931d8e3530
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2022-11-18 11:25:14 +0100

    changing syntax

--- Execice 13.py
+++ Execice 13.py
@@ -1,6 +1,6 @@
 r = int(input("donner le nombre des notes: "))
 z = 0
 for i in range(1, r + 1):
-    a = int(input(f"Donner le nombre n°{i} :"))
+    a = int(input(f"Donner la note n°{i} :"))
     z = z + a
 print("le moyen est:", z / r)

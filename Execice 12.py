e6b7405df393d0f0068073a2a568a6751a72435c
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2022-11-18 11:15:18 +0100

    give the position of the number

--- Execice 12.py
+++ Execice 12.py
@@ -1,6 +1,8 @@
 pg = 0
+z = 0
 for i in range(1, 11):
     a = int(input(f"Donner le nombre n°{i} :"))
     if a > pg:
         pg = a
-print(pg, "est le plus grand nombre")
+        z = i
+print(pg, "est le plus grand nombre, c'est le nombre n°,", z)

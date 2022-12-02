c7cd1c0352ac158e5463c7ba113d8d54905afc45
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2022-12-02 11:38:03 +0100

    Update Exercice 14.py

--- Exercice 14.py
+++ Exercice 14.py
@@ -1,1 +1,11 @@
-n=int(input())
+from re import I
+
+
+a = int(input('Donner a :'))
+b = int(input('Donner b :'))
+i = 1
+while i <= a and i <= b:
+    if a % i == 0 and b % i == 0:
+        PGCD = i
+    i += 1
+print(f"Le PGDC de {a} et {b} est {PGCD}")

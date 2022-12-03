a333925732def2dd31ef758f7c4d1ac5d7b8b0d0
MotriTN <gamerMT95@gmail.com>
2022-12-03 15:50:37 +0100

    Update Execice 15.py

--- Execice 15.py
+++ Execice 15.py
@@ -1,7 +1,7 @@
-a = int(input("Give a :"))
+a = int(input("Give the number to be reversed: "))
 i = 0
-while a != 0:
-    z = (a % 10) * (10)
-    a //= 10
-    i = z + a
-print(i)
+while(a > 0):
+ z = a %10
+ i = (i *10) + z
+ a = a //10
+print("Reverse of the provided number is = %d" %i)

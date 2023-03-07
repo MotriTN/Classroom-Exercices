79c31591ad6e21a469d994d231fa24c1b1340246
MotriTN <gamerMT95@gmail.com>
2023-03-07 20:55:17 +0100

    Update Exercice 4.py

--- trimestre 2/Exercice 4.py
+++ trimestre 2/Exercice 4.py
@@ -1,1 +1,25 @@
-matricule=input("Donner une matricule complete:  ")
+from random import randint
+n=str(randint(1000,9999))
+ch=input("Deviner un nombre de 4 chiffres: ")
+c=True
+t=0
+T=0
+V=0
+while c==True:
+    for i in range (0,4):
+        if ch[i]==n[i]:
+            T+=1
+        elif ch[i] in n[i:]:
+            V+=1
+    print("T=",T)
+    print("V=",V )
+    t+=1
+    if t==5 or T==4:
+        c=False
+    ch=input("Deviner un nombre de 4 chiffres: ")
+    T=0
+    V=0
+if T==4:
+    print("Bravo vous avez gagner")
+elif t==5:
+    print("Vous avez perdu")

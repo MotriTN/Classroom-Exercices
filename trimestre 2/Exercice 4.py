7d0e9905c8030f94c0cad9b65923ef6a4ba5f3c7
MotriTN <gamerMT95@gmail.com>
2023-03-07 21:26:54 +0100

    Update Exercice 4.py

--- trimestre 2/Exercice 4.py
+++ trimestre 2/Exercice 4.py
@@ -1,25 +1,22 @@
 from random import randint
 n=str(randint(1000,9999))
-ch=input("Deviner un nombre de 4 chiffres: ")
 c=True
 t=0
-T=0
-V=0
 while c==True:
+    ch=input("Deviner un nombre de 4 chiffres: ")
+    T=0
+    V=0
     for i in range (0,4):
         if ch[i]==n[i]:
             T+=1
-        elif ch[i] in n[i:]:
+        elif ch[i] in n:
             V+=1
     print("T=",T)
     print("V=",V )
     t+=1
-    if t==5 or T==4:
+    if t==6 or T==4:
         c=False
-    ch=input("Deviner un nombre de 4 chiffres: ")
-    T=0
-    V=0
 if T==4:
-    print("Bravo vous avez gagner")
-elif t==5:
-    print("Vous avez perdu")
+    print("Bravo vous avez gagner!")
+else :
+    print("Vous avez perdu!")

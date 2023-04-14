c74871f9198474a7b15473135edf0cf6f7382572
MotriTN <gamerMT95@gmail.com>
2023-04-14 16:04:15 +0100

    Update 0.Vocabulary.py

--- trimestre 3/0.Vocabulary.py
+++ trimestre 3/0.Vocabulary.py
@@ -1,2 +1,12 @@
 from numpy import *
+N=int(input("Donner la longeur du tableau: "))
+while not 5<=N<=30:
+    N=int(input("Donner la longeur du tableau: "))
 Tab= array([int]*N)
+for i in range (N):
+    S=int(input(f"Donner L'indice n°{i} de Tab: "))
+    while not S>0:
+        S=int(input(f"Donner L'indice n°{i} de Tab: "))
+    Tab[i]=S
+for j in range (N):
+    print(Tab[j])
